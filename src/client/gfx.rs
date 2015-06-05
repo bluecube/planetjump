265f7764ba9bf8db37ca01b6549376339c51d148
Kuba Marek <blue.cube@seznam.cz>
2015-06-05 08:48:33 +0200

    Some fixes.

--- src/client/gfx.rs
+++ src/client/gfx.rs
@@ -1,52 +1,52 @@
 use std::iter::Iterator;
 
-use sdl2::sdl::{init, InitBuilder};
-use sdl2::timer;
-use sdl2::video::{Window, WindowBuilder};
-use sdl2::render::{Renderer, RendererBuilder, RenderDrawer};
+extern crate sdl2;
+
+use sdl2::event::Event;
 use sdl2::keycode::KeyCode;
-use sdl2::event::{Event, EventPump};
 
 static FPS: u32 = 60;
 static FPS_SMOOTHING: f32 = 0.9;
 
 pub struct Gfx<'a> {
-    sdl: Sdl,
-    renderer: Renderer<'a>,
+    sdl: sdl2::Sdl,
+    renderer: sdl2::render::Renderer<'a>,
 }
 
 struct GfxLoopIterator<'a> {
-    event_pump: EventPump<'a>,
+    event_pump: sdl2::event::EventPump<'a>,
     last_tick: u32,
     fps: f32,
 }
 
 impl<'a> Gfx<'a> {
     pub fn new(title: &str) -> Gfx<'a> {
-        let sdl = init().video().unwrap();
-        let window = WindowBuilder::new(&sdl, title, 800, 600)
+        let sdl = sdl2::init().video().unwrap();
+        let window = sdl.window(title, 800, 600)
             .resizable()
-            .build().unwrap();
-        let renderer = RendererBuilder::new(window)
+            .build()
+            .unwrap();
+        let renderer = window.renderer()
             .accelerated()
             .present_vsync()
-            .build().unwrap();
+            .build()
+            .unwrap();
 
         Gfx { sdl: sdl,
               renderer: renderer }
     }
 
-    pub fn get_renderer(&self) -> &Renderer<'a> {
+    pub fn get_renderer(&self) -> &sdl2::render::Renderer<'a> {
         &self.renderer
     }
 
-    pub fn get_drawer<'b>(&'b mut self) -> RenderDrawer<'b> {
+    pub fn get_drawer<'b>(&'b mut self) -> sdl2::render::RenderDrawer<'b> {
         self.renderer.drawer()
     }
 
-    pub fn get_loop_iterator(&self) -> GfxLoopIterator {
+    pub fn get_loop_iterator(&mut self) -> GfxLoopIterator {
         GfxLoopIterator { event_pump: self.sdl.event_pump(),
-                          last_tick: timer::get_ticks(),
+                          last_tick: sdl2::timer::get_ticks(),
                           fps: FPS as f32 }
     }
 }
@@ -56,15 +56,15 @@ impl<'a> Iterator for GfxLoopIterator<'a> {
     type Item = (u32, f32);
 
     fn next(&mut self) -> Option<(u32, f32)> {
-        let now1 = timer::get_ticks();
+        let now1 = sdl2::timer::get_ticks();
         let elapsed1 = now1 - self.last_tick;
 
         let interval = 1000 / FPS;
         if elapsed1 < interval {
-            timer::delay(interval - elapsed1);
+            sdl2::timer::delay(interval - elapsed1);
         }
 
-        let now2 = timer::get_ticks();
+        let now2 = sdl2::timer::get_ticks();
         let elapsed2 = now2 - self.last_tick;
         self.last_tick = now2;
         let fps_raw = 1000.0 / (elapsed2 as f32);
