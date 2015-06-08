abeb12b38907f96674baed5ef50ed0390ae87366
Kuba Marek <blue.cube@seznam.cz>
2015-06-08 19:02:57 +0200

    Started working on generating the font.

---
+++ src/client/generate_font.py
@@ -0,0 +1,899 @@
+letters = r"""
+-a
+
+
+ XXXX
+X   X
+X   X
+X   X
+ XXXX
+
+-A
+  X
+ X X
+X   X
+X   X
+XXXXX
+X   X
+X   X
+
+-b
+X
+X
+XXXX
+X   X
+X   X
+X   X
+XXXX
+
+-B
+XXXX
+X   X
+X   X
+XXXX
+X   X
+X   X
+XXXX
+
+-c
+
+
+ XXX
+X   X
+X
+X   X
+ XXX
+
+-C
+ XXX
+X   X
+X
+X
+X
+X   X
+ XXX
+
+-d
+    X
+    X
+ XXXX
+X   X
+X   X
+X   X
+ XXXX
+
+-D
+XXXX
+X   X
+X   X
+X   X
+X   X
+X   X
+XXXX
+
+-e
+
+
+ XXX
+X   X
+XXXXX
+X
+ XXX
+
+-E
+XXXXX
+X
+X
+XXXX
+X
+X
+XXXXX
+
+-f
+ XX
+X
+X
+XX
+X
+X
+X
+
+-F
+XXXXX
+X
+X
+XXXX
+X
+X
+X
+
+-g
+
+
+ XXXX
+X   X
+X   X
+X   X
+ XXXX
+    X
+ XXX
+
+-G
+ XXX
+X   X
+X
+X  XX
+X   X
+X   X
+ XXX
+
+-h
+X
+X
+XXXX
+X   X
+X   X
+X   X
+X   X
+
+-H
+X   X
+X   X
+X   X
+XXXXX
+X   X
+X   X
+X   X
+
+-i
+X
+
+X
+X
+X
+X
+X
+
+-I
+XXX
+ X
+ X
+ X
+ X
+ X
+XXX
+
+-j
+  X
+
+  X
+  X
+  X
+  X
+XX
+
+-J
+ XXX
+   X
+   X
+   X
+   X
+X  X
+ XX
+
+-k
+X
+X
+X   X
+X  X
+XXX
+X  X
+X   X
+
+-K
+X   X
+X  X
+X X
+XXX
+X  X
+X   X
+X   X
+
+-l
+X
+X
+X
+X
+X
+X
+ XX
+
+-L
+X
+X
+X
+X
+X
+X
+XXXXX
+
+-m
+
+
+ XX XX
+X  X  X
+X  X  X
+X  X  X
+X  X  X
+
+-M
+X     X
+XX   XX
+X X X X
+X  X  X
+X     X
+X     X
+X     X
+
+-n
+
+
+ XxX
+X   X
+X   X
+X   X
+X   X
+
+-N
+X    X
+XX   X
+X X  X
+X  X X
+X   XX
+X    X
+X    X
+
+-o
+
+
+ XXX
+X   X
+X   X
+X   X
+ XXX
+
+-O
+ XXX
+X   X
+X   X
+X   X
+X   X
+X   X
+ XXX
+
+-p
+
+
+XXXX
+X   X
+X   X
+X   X
+XXXX
+X
+X
+
+-p
+XXXX
+X   X
+X   X
+XXXX
+X
+X
+X
+
+-q
+
+
+ XXXX
+X   X
+X   X
+X   X
+ XXXX
+    X
+    X
+
+-Q
+ XXX
+X   X
+X   X
+X   X
+X   X
+X X X
+ XXX
+   X
+    X
+
+-r
+
+
+ XXX
+X
+X
+X
+X
+
+-R
+XXXX
+X   X
+X   X
+XXXX
+X  X
+X   X
+X   X
+
+-s
+
+
+ XXXX
+X
+ XXX
+    X
+XXXX
+
+-S
+ XXX
+X   X
+X
+ XXX
+    X
+X   X
+ XXX
+
+-t
+X
+X
+XX
+X
+X
+X
+ XX
+
+-T
+XXXXX
+  X
+  X
+  X
+  X
+  X
+  X
+
+-u
+
+
+X   X
+X   X
+X   X
+X   X
+ XXX
+
+-U
+X   X
+X   X
+X   X
+X   X
+X   X
+X   X
+ XXX
+
+-v
+
+
+X   X
+X   X
+X   X
+ X X
+  X
+
+-V
+X   X
+X   X
+X   X
+X   X
+X   X
+ X X
+  X
+
+-w
+
+
+X       X
+X       X
+X   X   X
+ X X X X
+  X   X
+
+-W
+X       X
+X       X
+X       X
+X   X   X
+ X  X  X
+ X X X X
+  X   X
+
+-x
+
+
+X   X
+ X X
+  X
+ X X
+X   X
+
+-X
+X   X
+X   X
+ X X
+  X
+ X X
+X   X
+X   X
+
+-y
+
+
+X   X
+X   X
+X   X
+ X X
+  X
+ X
+
+-Y
+X   X
+X   X
+ X X
+  X
+  X
+  X
+  X
+
+-z
+
+
+XXXXX
+   X
+  X
+ X
+XXXXX
+
+-Z
+XXXXX
+    X
+   X
+  X
+ X
+X
+XXXXX
+
+-!
+X
+X
+X
+X
+X
+
+X
+
+-"
+X X
+X X
+
+-#
+
+
+ # #
+#####
+ # #
+#####
+ # #
+
+-$
+ XXX
+X X X
+X X
+ XXX
+  X X
+X X X
+ XXX
+
+-%
+
+
+X   X
+   X
+  X
+ X
+X   X
+
+-&
+  XX
+ X  X
+ X  X
+  XX
+ X X  X
+X   X
+ XXX X
+
+-"
+X
+X
+
+-(
+  X
+ X
+X
+X
+X
+ X
+  X
+
+-(
+X
+ X
+  X
+  X
+  X
+ X
+X
+
+-*
+
+X X X
+ XXX
+XXXXX
+ XXX
+X X X
+
+-+
+
+  X
+  X
+XXXXX
+  X
+  X
+
+-,
+
+
+
+
+
+
+ X
+X
+
+--
+
+
+
+XXXXX
+
+-.
+
+
+
+
+
+
+X
+
+-/
+
+  X
+  X
+ X
+ X
+X
+X
+
+-0
+ XXX
+X   X
+X  XX
+X X X
+XX  X
+X   X
+ XXX
+
+-1
+ X
+XX
+ X
+ X
+ X
+ X
+XXX
+
+-2
+ XXX
+X   X
+    X
+   X
+  X
+ X
+XXXXX
+
+-3
+ XXX
+X   X
+    X
+   X
+    X
+X   X
+ XXX
+
+-4
+    X
+   XX
+  X X
+ X  X
+XXXXX
+    X
+    X
+
+-5
+XXXXX
+X
+XXXX
+    X
+    X
+X   X
+ XXX
+
+-6
+ XXX
+X   X
+X
+XXXX
+X   X
+X   X
+ XXX
+
+-7
+XXXXX
+    X
+   X
+  X
+ X
+ X
+ X
+
+-8
+ XXX
+X   X
+X   X
+ XXX
+X   X
+X   X
+ XXX
+
+-9
+ XXX
+X   X
+X   X
+ XXXX
+    X
+X   X
+ XXX
+
+-:
+
+
+X
+
+
+X
+
+
+-;
+
+
+ X
+
+
+ X
+X
+
+-<
+
+   XX
+ XX
+X
+ XX
+   XX
+
+
+-=
+
+
+XXXXX
+
+XXXXX
+
+->
+
+XX
+  XX
+    X
+  XX
+XX
+
+-?
+ XXX
+X   X
+    X
+   X
+  X
+
+  X
+
+-@
+
+   XXX
+ XX   X
+X  XXX X
+X X  X X
+X X  X X
+X  XXXX
+
+-[
+XXX
+X
+X
+X
+X
+X
+XXX
+
+-]
+XXX
+  X
+  X
+  X
+  X
+  X
+XXX
+
+-\
+
+X
+X
+ X
+ X
+  X
+  X
+
+-^
+  X
+ X X
+
+-_
+
+
+
+
+
+
+
+XXXXX
+
+-`
+X
+ X
+
+-{
+  X
+ X
+ X
+X
+ X
+ X
+  X
+
+-}
+X
+ X
+ X
+  X
+ X
+ X
+X
+
+-|
+X
+X
+X
+X
+X
+X
+X
+
+-~
+
+
+ X
+X X X
+   X
+
+"""
+
+ascent = 7
+descent = 2
+spacing = 1
+space_advance = 4
+
+import re
+
+def header_and_rest(fn, iterable):
+    current_header = None
+    current_group = None
+    for item in iterable:
+        header = fn(iterable)
+        if header:
+            if current_header:
+                yield (current_header, current_group)
+            current_header = header
+            current_group = []
+        else:
+            if not current_header:
+                raise RuntimeError("Group without header")
+            current_group.append(item)
+    if current_header:
+        yield (current_header, current_group)
+
+def match_header(line):
+    match = re.match("^-(.)\s*$", line)
+    if not match:
+        return False
+    else:
+        return match.group(1)
+
+def line_to_number(line):
+    ret = 0
+    used = 0
+    for i, char in enumerate(line):
+        if char.isspace():
+            ret = ret | 1 << i
+            used = i + 1
+
+    return (ret, used)
+
+letters_map = {}
+
+letters_map[" "] = ([0] * (ascent + descent), space_advance)
+
+for letter, lines in header_and_rest(match_header, letters.split("\n")):
+    numbers, width = zip(*map(line_to_number, lines))
+    width = max(widht)
+
+    if letter in letters_map:
+        raise RuntimeError("Duplicate letter " + repr(letter))
+
+    if any(number > 255 for number in numbers):
+        raise RuntimeError("Letter " + repr(letter) + " is wider than 8 characters")
+
+    if len(numbers) < ascent + descent:
+        numbers.extend([0] * (ascent + descent - len(numbers)))
+    elif len(numbers) > ascent + descent:
+        raise RuntimeError("Letter " + repr(letter) + " is taller than ascent + descent (" + (ascent + descent) + ")")
+
+    letters_map[letter] = (numbers, width + spacing)
+
+
+for i in range(ord(" "), ord("~") + 1):
+    c = chr(i)
+    if c not in letters_map:
+        raise Exception("Missing letter " + repr(c))
+
+    print("/* '{}' */ {:03i}, [".format(c, letters_map[c][1]) + ", ".join("{:03i}", number for number in lettes_map[c][0]) + "]")
