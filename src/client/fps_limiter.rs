bc9242aefe6bb3e2147738986b4f9cbbb7c10ab5
Kuba Marek <blue.cube@seznam.cz>
2015-06-28 13:03:07 +0200

    Adaptive framerate limiter based on a PI controller.

--- src/client/fps_limiter.rs
+++ src/client/fps_limiter.rs
@@ -1,10 +1,14 @@
 extern crate sdl2;
 
-static FPS_SMOOTHING: f32 = 0.99;
+const FPS_SMOOTHING: f32 = 0.99;
+const KP: f32 = 0.5;
+const KI: f32 = 0.5;
+const WINDUP_LIMIT: f32 = 1000.0;
 
 pub struct FpsLimiter {
-    target_frame_time: u32,
+    target_frame_time: f32,
     last_tick: u32,
+    integration_buffer: f32,
     fps: f32,
 }
 
@@ -13,20 +17,31 @@ impl Iterator for FpsLimiter {
     type Item = (u32, f32);
 
     fn next(&mut self) -> Option<(u32, f32)> {
-        let now1 = sdl2::timer::get_ticks();
-        let elapsed1 = now1 - self.last_tick;
+        let now = sdl2::timer::get_ticks();
+        let elapsed = (now - self.last_tick) as f32;
+        let error = self.target_frame_time - elapsed;
 
-        if elapsed1 < self.target_frame_time {
-            sdl2::timer::delay(self.target_frame_time as u32 - elapsed1);
+        self.integration_buffer += error; // TODO: shouldn't we use error * elapsed / target_frame_time?
+        if self.integration_buffer > WINDUP_LIMIT {
+            self.integration_buffer = WINDUP_LIMIT;
+        }
+        else if self.integration_buffer < -WINDUP_LIMIT {
+            self.integration_buffer = -WINDUP_LIMIT;
         }
 
-        let now2 = sdl2::timer::get_ticks();
-        let elapsed2 = now2 - self.last_tick;
-        self.last_tick = now2;
-        let fps_raw = 1000.0 / (elapsed2 as f32);
+        self.last_tick = now;
+
+        let to_sleep = (KP * error + KI * self.integration_buffer).round();
+        if to_sleep > 0.0 {
+            sdl2::timer::delay(to_sleep as u32);
+        }
+
+        let fps_raw = 1000.0 / if elapsed > 0.0 { elapsed } else { 0.5 };
+            // If elapsed is 0, we assume that it is a rounding error, and it
+            // should be 0.5
         self.fps = (1.0 - FPS_SMOOTHING) * fps_raw + FPS_SMOOTHING * self.fps;
 
-        Some((elapsed2, self.fps))
+        Some((elapsed as u32, self.fps))
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -36,8 +51,10 @@ impl Iterator for FpsLimiter {
 
 impl FpsLimiter {
     pub fn new(target_fps: u32) -> FpsLimiter {
-        FpsLimiter { target_frame_time: (1000.0 / (target_fps as f32)) as u32,
-                     last_tick: sdl2::timer::get_ticks(),
+        let target_frame_time = 1000.0 / (target_fps as f32);
+        FpsLimiter { target_frame_time: target_frame_time,
+                     last_tick: sdl2::timer::get_ticks() - (target_frame_time / 2.0).round() as u32,
+                     integration_buffer: 0.0,
                      fps: target_fps as f32 }
     }
 }
